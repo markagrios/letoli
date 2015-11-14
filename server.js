3b0abb85e8b7d5ff4a0337652aaa4a0aff62ac3f
Mark Agrios <magrios@gmail.com>
2015-11-14 14:09:25 -0500

    easiest server thing I've ever done. next step is to figure out how to put it on heroku

---
+++ server.js
@@ -0,0 +1,27 @@
+/**
+ * Static HTTP Server
+ *
+ * Create a static file server instance to serve files
+ * and folder in the './public' folder
+ */
+
+// modules
+var static = require( 'node-static' ),
+    port = 8080,
+    http = require( 'http' );
+
+// config
+var file = new static.Server( './public', {
+    cache: 3600,
+    gzip: true
+} );
+
+// serve
+http.createServer( function ( request, response ) {
+    request.addListener( 'end', function () {
+        file.serve( request, response );
+    } ).resume();
+} ).listen( port );
+
+
+console.log("running on port " + port);
