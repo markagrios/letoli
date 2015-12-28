5fc25264ff1e00ea83b36cc12d60ce75ba1a1892
Mark Agrios <magrios@gmail.com>
2015-12-28 17:52:42 -0500

    numericjs works. I can multiply matrices

--- public/evolve.js
+++ public/evolve.js
@@ -1,19 +1,45 @@
 console.log("evolve.js");
 
-function sigmoid(t) {
+// Hyperparameters
+
+// Lessthanhyper-parameters, I don't even know
+var BIAS = 0.5;
+var THRESHOLD = 2;
+
+
+
+function sigmoid(t) { // possibly not even necessary
     return 1/(1+Math.pow(Math.E, -t));
 }
 
-
-var neuron = {
+function neuron(a) {	
+	// sum of weighted inputs plus a bias evaluated at a parameter.
+	// sum(XiWi) - bias > parameter
 	
-};
+	// a is an array
+	
+	//output weighted inputs if meets condition, output 0 if not
 
+	var A = numeric.sum(a);
+	
+	if(A - BIAS > THRESHOLD) {
+		return A;
+	} else {
+		return 0;
+	}
+	
+	
+}
 
+var X = [.75,.50,.90];
+var W = [
+		[1,4,6,2],
+		[6,3,8,1],
+		[4,2,7,2]
+		];
 
-
-
-
+var z = numeric.dot(X,W);
+console.log(z);
 
 
 
