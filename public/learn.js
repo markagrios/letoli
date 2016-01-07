2819425a6b3d89d735c10df2411cdf99dd61a857
Mark Agrios <magrios@gmail.com>
2016-01-07 14:57:23 -0500

    added notes.

--- public/learn.js
+++ public/learn.js
@@ -10,25 +10,7 @@ const PERTURBCHANCE = 0.2;
 var bias = 0.14;
 var threshold = 0.4;
 
-var Q = [ 
-		[9,4,6,2,6],
-		[1,3,8,1,7],
-		[4,2,7,2,9],
-		[3,7,5,1,8]
-		]; // 4x5
-
-
-function vote(array) {  // returns the index of the highest value in array
-	var winner = 0;
-	var best = array[0];
-	for(var i = 0; i < array.length; i++) {
-		if(array[i] > best) {
-			best = array[i];
-			winner = i;
-		}
-	}
-	return winner;
-}
+var 
 
 function addColumn(a,b) { // add b to be a new column of a
 	at = numeric.transpose(a);
@@ -56,7 +38,7 @@ function activity(a) { // input matrix, return matrix
 	return a; // ?
 }
 
-function vary(a) { // input matrix, return matrix
+function vary(a) { // used on weight matrices
 	for(var i = 0; i < a.length; i++) {
 		for(var j = 0; j < a[i].length; j++) {
 			var change = ((Math.random() / 2) - 0.25).toFixed(2) * 1; // makes changes between -.25 and .25
@@ -72,6 +54,19 @@ function forward(X) {
 	var X = [letoli.food, letoli.water, letoli.sleep];
 	var Y;
 	
+	return Y;
+}
+
+function vote(array) {  // returns the index of the highest value in array
+	var winner = 0;
+	var best = array[0];
+	for(var i = 0; i < array.length; i++) {
+		if(array[i] > best) {
+			best = array[i];
+			winner = i;
+		}
+	}
+	return winner;
 }
 
 function choose(array) {
@@ -87,16 +82,16 @@ function choose(array) {
 
 	switch(action) {
 		case MUP: 
-			letoli.move(0,-10);
+			letoli.move(0,-20);
 		break;
 		case MDOWN: 
-			letoli.move(0,10);
+			letoli.move(0,20);
 		break;
 		case MLEFT: 
-			letoli.move(-10,0);
+			letoli.move(-20,0);
 		break;		
 		case MRIGHT: 
-			letoli.move(10,0);
+			letoli.move(20,0);
 		break;
 		case EAT: 
 			letoli.eat();
@@ -108,9 +103,10 @@ function choose(array) {
 			letoli.rest();
 		break;
 		default:
-			// do nothing
+			console.log("For some reason, he can't decide.");
 	}
 }
+
 
 
 
@@ -124,18 +120,24 @@ var A = [
 
 
 
-//////////////THIS IS THE END OF THE CODE, STEP IS ONE PASS THROUGH THE Neutral NETWORK//////////////// 
+//////////////THIS IS THE END OF THE CODE, STEP IS ONE PASS THROUGH THE Neural NETWORK//////////////// 
 
 function live() {
 	letoli.decrement();
-	
-	
+	X = [letolo.food, letoli.water, letoli.sleep];
+	choose(forward(X));
 }
 
 //setInterval(live, 1000);
 
 
 
-
-
+/* NOTES
+ * 
+ * when and when not to change weights. How do I know what was just changed should be kept
+ * and what should be changed more.
+ * 
+ * How do I decide to add a new neuron/layer?
+ * 
+*/
 
